302fb4cc585b1bc2c6c9f2657db2794d56d46386
¨stevmager¨ <stev@dot.net>
2014-01-21 11:12:24 +0100

    Update1

--- src/main/java/stev/App.java
+++ src/main/java/stev/App.java
@@ -6,7 +6,7 @@ public class App
     public static void main( String[] args )
     {
 	for (int i = 0; i < 10; i++){
-        System.out.println( "Hello World!" );
+        System.out.println( "Hellooooo World!" );
 	}
 
     }
