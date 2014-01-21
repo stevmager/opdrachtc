04be780373585e1da31f3fce8eea7d3571c822df
stevmager <stev@dot.net>
2014-01-21 13:00:09 +0100

    ¨jenkins¨

--- src/main/java/stev/App.java
+++ src/main/java/stev/App.java
@@ -6,7 +6,7 @@ public class App
     public static void main( String[] args )
     {
 	for (int i = 0; i < 24; i++){
-        System.out.println( "Hello433233243oooo World!" );
+        System.out.println( "Hello World!" );
 	}
 
     }
