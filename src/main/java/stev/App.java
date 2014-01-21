1ea8cea29ab3ef9acf4f567cb7cdb6a4ffc741e4
stevmager <stev@dot.net>
2014-01-21 11:02:01 +0100

    ¨jenkins¨

---
+++ src/main/java/stev/App.java
@@ -0,0 +1,13 @@
+package stev;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+	for (int i = 0; i < 4; i++){
+        System.out.println( "Hello World!" );
+	}
+
+    }
+}
